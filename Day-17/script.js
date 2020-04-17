45f170d2d23eb5d11958df2b13b834f5c380c110
sescobar99 <santi.e.c@hotmail.com>
2020-04-17 12:27:44 -0500

    day 17

---
+++ Day-17/script.js
@@ -0,0 +1,22 @@
+let artists;
+const myKey = '3ebe0127cde5f85fe25a8eae5d5e9f89'; //:( 🕵️🚓👮
+const api = `http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${myKey}&format=json`
+
+const ul = document.querySelector('#bands');
+function stripArticles(word) {
+    return word.replace(/^(a |the |an )/i, '').trim();
+}
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+
+fetch(api)
+    .then(blob => blob.json())
+    .then(data => {
+        artists = data.artists.artist;
+        artists.sort((a, b) => stripArticles(a.name) > stripArticles(b.name) ? 1 : -1);
+        ul.innerHTML = artists.map(artist =>
+            `<li><span><a href="${artist.url}" target="_blank">🔗</a>${artist.name}</span> <span>${numberWithCommas(artist.listeners)}🎧</span></li>`
+        ).join('\n');
+    });
