913b4e5dea51aa5e1baaf76cc97f636fdd676c39
sescobar99 <santi.e.c@hotmail.com>
2020-04-21 18:08:48 -0500

    compass init

--- Day-21/script.js
+++ Day-21/script.js
@@ -6,16 +6,26 @@ const lon = document.querySelector('.lon');
 
 window.navigator.geolocation.watchPosition((data) => {
   console.log(data);
-  speed.textContent = roundTwoDec(data.coords.speed) ;
-  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  speed.textContent = roundTwoDec(data.coords.speed);
+  // arrow.style.transform = `rotate(${data.coords.heading}deg)`;
   lat.textContent = roundTwoDec(data.coords.latitude);
   lon.textContent = roundTwoDec(data.coords.longitude);
 
 }, (err) => {
-    console.log(err);
+  console.log(err);
   alert('Please enable geolocation');
 });
 
-function roundTwoDec(number){
-  return Math.round(( number + Number.EPSILON) * 100) / 100;
+function roundTwoDec(number) {
+  return Math.round((number + Number.EPSILON) * 100) / 100;
+}
+
+
+if (window.DeviceOrientationEvent) {
+  window.addEventListener("deviceorientation", function (event) {
+    // alpha: rotation around z-axis
+    var rotateDegrees = event.alpha;
+    arrow.style.transform = `rotate(${360-rotateDegrees}deg)`;
+    
+  }, true);
 }
