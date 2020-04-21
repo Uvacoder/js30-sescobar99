aed7cd321a36b010b758177277edce32caf8e3cd
sescobar99 <santi.e.c@hotmail.com>
2020-04-20 22:01:36 -0500

    day 20 code cleaning

--- Day-20/script.js
+++ Day-20/script.js
@@ -1,5 +1,18 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const songs = new Map([
+    ['Himno', 'https://www.youtube.com/watch?v=UkW9oMMdHwk'],
+    ['Secret', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'],
+    ['Party', 'https://www.youtube.com/watch?v=6Zbi0XmGtMw'],
+    ['Rap', 'https://www.youtube.com/watch?v=dY2sfuA1UPc'],
+    ['Vampire', 'https://www.youtube.com/watch?v=3RTIXUzCr2M'],
+    ['Festín', 'https://www.youtube.com/watch?v=NQNzAkBF4uw'],
+    ['Rasputín', 'https://www.youtube.com/watch?v=kvDMlk3kSYg'],
+    ['Rock it for me', 'https://www.youtube.com/watch?v=fBGSJ3sbivI'],
+    ['Scatman', 'https://www.youtube.com/watch?v=Hy8kmNEo1i8'],
+    ['Buttercup', 'https://www.youtube.com/watch?v=cCWNbYwEfdw']
+]);
+const table = document.getElementById('table');
 
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
@@ -12,49 +25,28 @@ recognition.addEventListener('result', e => {
         .map(result => result[0])
         .map(result => result.transcript)
         .join('');
-
     p.textContent = transcript;
     if (e.results[0].isFinal) {
         p = document.createElement('p');
         words.appendChild(p);
     }
-
     let aux = transcript.toLowerCase();
-
-    if (aux.includes('rasputin') || aux.includes('rasputín')) {
-        window.open('https://www.youtube.com/watch?v=kvDMlk3kSYg', '_blank');
-        recognition.abort();
-    } else if (aux.includes('scatman')) {
-        window.open('https://www.youtube.com/watch?v=Hy8kmNEo1i8', '_blank');
-        recognition.abort();
-    } else if (aux.includes('rock it for me')) {
-        window.open('https://www.youtube.com/watch?v=fBGSJ3sbivI', '_blank');
-        recognition.abort();
-    } else if (aux.includes('vampire')) {
-        window.open('https://www.youtube.com/watch?v=3RTIXUzCr2M', '_blank');
-        recognition.abort();
-    } else if (aux.includes('party')) {
-        window.open('https://www.youtube.com/watch?v=6Zbi0XmGtMw', '_blank');
-        recognition.abort();
-    } else if (aux.includes('himno')) {
-        window.open('https://www.youtube.com/watch?v=UkW9oMMdHwk', '_blank');
-        recognition.abort();
-    } else if (aux.includes('secret')) {
-        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
-        recognition.abort();
-    } else if (aux.includes('rap')) {
-        window.open('https://www.youtube.com/watch?v=dY2sfuA1UPc', '_blank');
-        recognition.abort();
-    } else if (aux.includes('festin') || aux.includes('festín')) {
-        window.open('https://www.youtube.com/watch?v=NQNzAkBF4uw', '_blank');
-        recognition.abort();
-    }
-    // console.log(transcript);
-
+    songs.forEach((value, key) => {
+        if (aux.includes(key.toLowerCase())) {
+            window.open(value, '_blank');
+            recognition.abort();
+        }
+    });
+    console.log(transcript);
 });
 
-
 recognition.addEventListener('end', recognition.start);
-
 recognition.start();
 
+const ul = document.createElement('ul');
+songs.forEach((value, key) => {
+    const li = document.createElement('li');
+    li.innerText = key;
+    ul.appendChild(li);
+});
+table.appendChild(ul);
