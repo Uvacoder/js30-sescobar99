13fbdde2e0b5150e60cd9672d8bb009558d006c0
sescobar99 <santi.e.c@hotmail.com>
2020-04-21 17:56:00 -0500

    day 21 lat lon

--- Day-21/script.js
+++ Day-21/script.js
@@ -1,12 +1,21 @@
 const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
+const lat = document.querySelector('.lat');
+const lon = document.querySelector('.lon');
 
-navigator.geolocation.watchPosition((data) => {
-  // console.log(data);
-  speed.textContent = data.coords.speed;
+
+window.navigator.geolocation.watchPosition((data) => {
+  console.log(data);
+  speed.textContent = roundTwoDec(data.coords.speed) ;
   arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  lat.textContent = roundTwoDec(data.coords.latitude);
+  lon.textContent = roundTwoDec(data.coords.longitude);
 
 }, (err) => {
-    // console.log(err);
+    console.log(err);
   alert('Please enable geolocation');
 });
+
+function roundTwoDec(number){
+  Math.round(( number + Number.EPSILON) * 100) / 100
+}
