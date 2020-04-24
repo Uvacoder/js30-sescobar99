b40a3ac1c26b5f0ab10e4c8d0f0e591a09ae0991
sescobar99 <santi.e.c@hotmail.com>
2020-04-24 15:13:52 -0500

    day 24 portfolio update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const daysFinished = 23;
+const daysFinished = 24;
 const taskName = ['Drum Kit', 'JS and CSS Clock', 'CSS Variables', 'Array Cardio Day 1', 'Flex Panel Gallery', 'Type Ahead', 'Array Cardio Day 2', 'Fun with HTML5 Canvas', 'Dev Tools Domination', 'Hold Shift and Check Checkboxes', 'Custom Video Player', 'Key Sequence Detection', 'Slide in on Scroll', 'JavaScript References VS Copying', 'LocalStorage', 'Mouse Move Shadow', 'Sort Without Articles', 'Adding Up Times with Reduce', 'Webcam Fun', 'Speech Detection', 'Geolocation', 'Follow Along Link Highlighter', 'Speech Synthesis', 'Sticky Nav', 'Event Capture, Propagation, Bubbling and Once', 'Stripe Follow Along Nav', 'Click and Drag', 'Video Speed Controller', 'Countdown Timer', 'Whack A Mole']
 
 // Change style of navbar on scroll
